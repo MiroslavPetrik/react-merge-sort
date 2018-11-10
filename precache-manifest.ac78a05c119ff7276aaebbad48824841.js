7c396c03c7c4b1608e437b0c3403ae6b90da319f
Miroslav Petrik <miroslav.petrik@thinkcreatix.com>
2018-11-10 22:49:59 +0100

    Updates

---
+++ precache-manifest.ac78a05c119ff7276aaebbad48824841.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e263ac809990bbff3b0",
+    "url": "/static/js/main.8e263ac8.chunk.js"
+  },
+  {
+    "revision": "aff434dca421bc75b118",
+    "url": "/static/js/1.aff434dc.chunk.js"
+  },
+  {
+    "revision": "8e263ac809990bbff3b0",
+    "url": "/static/css/main.0469c5a2.chunk.css"
+  },
+  {
+    "revision": "167239a52c6b932470e9a30e2b3bc2c6",
+    "url": "/index.html"
+  }
+];
